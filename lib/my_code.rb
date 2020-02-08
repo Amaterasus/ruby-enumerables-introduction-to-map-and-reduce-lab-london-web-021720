1763b23e7d844fb3d195c32f9a8ed713b9c76480
Skye Geraghty <skyetay03@googlemail.com>
2020-02-08 16:01:08 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,76 @@
 # My Code here....
+
+
+def map_to_negativize(source)
+  index = 0
+  result = []
+  
+  while index < source.length do 
+    result.push(source[index] * -1)
+    
+    index += 1 
+  end
+  
+  return result
+end
+
+def map_to_no_change(source)
+  source
+end
+
+def map_to_double(source)
+  index = 0
+  result =[]
+  
+  while index < source.length do 
+    result.push(source[index] * 2)
+    index += 1
+  end
+  
+  return result
+end
+
+def map_to_square(source)
+  index = 0
+  result = []
+  
+  while index < source.length do
+    result.push(source[index]*source[index])
+    index += 1
+  end
+  return result
+end
+
+
+def reduce_to_total(source, total = 0)
+  index = 0
+  
+  while index < source.length do 
+    total += source[index]
+    index += 1
+  end
+  
+  return total
+end
+
+def reduce_to_all_true(source)
+  index = 0
+  while index < source.length do 
+    if !source[index]
+      return false
+    end
+    index += 1
+  end
+  return true
+end
+
+def reduce_to_any_true(source)
+  index = 0
+  while index < source.length do 
+    if source[index]
+      return true
+    end
+    index += 1
+  end
+  return false
+end
